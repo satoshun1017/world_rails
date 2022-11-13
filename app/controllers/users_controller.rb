b672148ac38c3e397a83e64f749808732c0a92c8
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2022-11-13 13:32:53 +0900

    CRUD処理の実装【Read 編】

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def index
+    binding.pry
     @users = User.all
 
     render json: @users
+  end
+
+  def show
+    # binding.pry
+    @user = User.find(params[:id])
   end
 end
